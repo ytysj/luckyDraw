699776b7bb05b2adce4d2acec5e5b8d72bf05951
YouChenhui <chyou@raysentek.com>
2024-02-01 13:56:22 +0800

    更新：新增test文件人数，添加单次抽选人数数量，添加中文打印

--- js/index.js
+++ js/index.js
@@ -12,6 +12,10 @@ var fourth_gift_num = 0;//四等奖数量
 var third_gift_num = 0;//三等奖数量
 var second_gift_num = 0;//二等奖数量
 var first_gift_num = 0;//一等奖数量
+var fourth_once_num = 1;//四等奖每轮抽取的次数
+var third_once_num = 1;//三等奖每轮抽取的次数
+var second_once_num = 1;//二等奖每轮抽取的次数
+var first_once_num = 1;//一等奖每轮抽取的次数
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -43,25 +47,31 @@ $(function () {
         else {
             //确定奖品类型            
             var display_id = "gift4";
+            var cur_get_num = 1; //当前抽选次数
+            
             console.log("正在抽取cur_gift_idx：", cur_gift_idx, fourth_gift_num+third_gift_num+second_gift_num, 
                         third_gift_num+second_gift_num+first_gift_num);
+            console.log("当前抽选次数:", cur_get_num);
             if(cur_gift_idx<fourth_gift_num){
+                cur_get_num = fourth_once_num;
                 display_id = "gift4";
-                $("#HintMessage").val("已抽选四等奖("+(cur_gift_idx+1)+"/"+fourth_gift_num+")");
+                $("#HintMessage").val("已抽选四等奖("+(cur_gift_idx+cur_get_num)+"/"+fourth_gift_num+")");
 
                 document.getElementById("giftTitle4").className="giftTitle"; //把奖品标题的class，从hide设置为giftTitle
             }
             else if(cur_gift_idx<fourth_gift_num+third_gift_num){
+                cur_get_num = third_once_num;
                 display_id = "gift3";
-                $("#HintMessage").val("已抽选三等奖("+(cur_gift_idx+1-fourth_gift_num)+"/"+third_gift_num+")");
+                $("#HintMessage").val("已抽选三等奖("+(cur_gift_idx+cur_get_num-fourth_gift_num)+"/"+third_gift_num+")");
 
                 document.getElementById("giftTitle3").className="giftTitle"; //把奖品标题的class，从hide设置为giftTitle
                 document.getElementById("giftTitle4").className="hide";//隐藏四等奖
                 hide_all_element("gift4");
             }
             else if(cur_gift_idx<fourth_gift_num+third_gift_num+second_gift_num){
+                cur_get_num = second_once_num;
                 display_id = "gift2";
-                $("#HintMessage").val("已抽选二等奖("+(cur_gift_idx+1-fourth_gift_num-third_gift_num)+"/"+second_gift_num+")");
+                $("#HintMessage").val("已抽选二等奖("+(cur_gift_idx+cur_get_num-fourth_gift_num-third_gift_num)+"/"+second_gift_num+")");
 
                 document.getElementById("giftTitle2").className="giftTitle"; //把奖品标题的class，从hide设置为giftTitle
                 document.getElementById("giftTitle4").className="hide";//隐藏四等奖和三等奖
@@ -70,8 +80,9 @@ $(function () {
                 hide_all_element("gift3");
             }
             else if(cur_gift_idx<fourth_gift_num+third_gift_num+second_gift_num+first_gift_num){
+                cur_get_num = first_once_num;
                 display_id = "gift1";
-                $("#HintMessage").val("已抽选一等奖("+(cur_gift_idx+1-fourth_gift_num-third_gift_num-second_gift_num)+"/"+first_gift_num+")");
+                $("#HintMessage").val("已抽选一等奖("+(cur_gift_idx+cur_get_num-fourth_gift_num-third_gift_num-second_gift_num)+"/"+first_gift_num+")");
 
                 document.getElementById("giftTitle1").className="giftTitle"; //把奖品标题的class，从hide设置为giftTitle
                 document.getElementById("giftTitle4").className="hide";//隐藏四等奖和三等奖
@@ -80,10 +91,9 @@ $(function () {
                 hide_all_element("gift3");
             }
             else{
-                console.log("WARNING!cur_gift_idx out of range",cur_gift_idx, third_gift_num, second_gift_num, first_gift_num);
+                console.log("WARNING!cur_gift_idx out of range",cur_get_num, cur_gift_idx, fourth_gift_num, third_gift_num, second_gift_num, first_gift_num);
                 return null;
             }
-            console.log("display_id:", display_id);
 
             //设置样式
             $("#btnStart").text("开始");//设置按钮文本为开始
@@ -91,46 +101,46 @@ $(function () {
             $("#bgLuckyDrawEnd").addClass("bg");//添加中奖背景光辉
 
             //抽选人员
-            var selectedPerson = getRandomPerson();
-            var num = getPersonNum(selectedPerson);
-            var name = getPersonName(selectedPerson);
-            var group = getPersonGroup(selectedPerson);
-            var suffix = group+num;
-            console.log("num:", num, "name:", name, "group:", group);
+            for(var i=0;i<cur_get_num;i++){
+                var selectedPerson = getRandomPerson();
+                var num = getPersonNum(selectedPerson);
+                var name = getPersonName(selectedPerson);
+                var group = getPersonGroup(selectedPerson);
+                var suffix = group+num;
+                console.log("抽选人员 工号:", num, "姓名:", name, "部门:", group);
 
-            //在跳动页面显示人名
-            $("#showName").val(name);
+                //在跳动页面显示人名
+                $("#showName").val(name);
 
-            //添加到入选名单
-            var element=document.getElementById(display_id);
-            var newBox=document.createElement("span");
-            newBox.className = "luckyBox";
+                //添加到入选名单
+                var element=document.getElementById(display_id);
+                var newBox=document.createElement("span");
+                newBox.className = "luckyBox";
 
-            var newEle=document.createElement("div");
-            newEle.innerHTML=name;//添加名字
-            newEle.className = "luckyName";
-            newBox.appendChild(newEle);
+                var newEle=document.createElement("div");
+                newEle.innerHTML=name;//添加名字
+                newEle.className = "luckyName";
+                newBox.appendChild(newEle);
 
-            var newEle=document.createElement("div");
-            newEle.className = "luckySuffix";
-            newEle.innerHTML=suffix;//添加后缀
-            newBox.appendChild(newEle);
+                var newEle=document.createElement("div");
+                newEle.className = "luckySuffix";
+                newEle.innerHTML=suffix;//添加后缀
+                newBox.appendChild(newEle);
 
-            element.appendChild(newBox);
+                element.appendChild(newBox);
+            }
 
-            cur_gift_idx += 1;
+            cur_gift_idx += cur_get_num;
         }
     });
 
     $("#btnCfg").on("click", function () {
         if(cfg_page_visible==false){
-            console.log("visible false")
             $("#cfgPage").fadeIn();
             cfg_page_visible = true;
             $("#btnCfg").text("应用");
         }
         else{
-            console.log("visible true")
             showConfirm("确认应用配置参数吗？会导致重置！", function () {
                 //加载人员名单
                 if(loaded_person_file==null){
@@ -144,10 +154,15 @@ $(function () {
                 cur_gift_idx = 0;   //当前在抽第几个奖                
                 all_person_num = allPerson.length;  //获取员工总人数
                 fourth_gift_num = Number($("#numInput4").val()); //获取奖品数量
-                third_gift_num = Number($("#numInput3").val()); //获取奖品数量
+                third_gift_num = Number($("#numInput3").val());
                 second_gift_num = Number($("#numInput2").val());
                 first_gift_num = Number($("#numInput1").val());
-                console.log("gift_num:",fourth_gift_num, third_gift_num, second_gift_num, first_gift_num);
+                fourth_once_num = Number($("#onceNumInput4").val()); //获取奖品单次抽奖数量
+                third_once_num = Number($("#onceNumInput3").val());
+                second_once_num = Number($("#onceNumInput2").val());
+                first_once_num = Number($("#onceNumInput1").val());
+                console.log("礼物数量:",fourth_gift_num, third_gift_num, second_gift_num, first_gift_num);
+                console.log("单次抽奖数量:",fourth_once_num, third_once_num, second_once_num, first_once_num);
 
                 //重置页面
                 $("#rollingName").fadeOut();
@@ -160,7 +175,6 @@ $(function () {
 
                 //删除所有抽奖人名
                 elements = document.querySelectorAll(".luckyBox");// 获取所有指定类名为"luckyBox"的元素
-                console.log("delete all luckyBox", elements)
                 for (var i = 0; i < elements.length; i++) { // 遍历每个元素并移除它们
                     var element = elements[i];
                     element.parentNode.removeChild(element); // 或者直接使用 remove() 方法
@@ -169,6 +183,7 @@ $(function () {
                 $("#cfgPage").fadeOut();
                 cfg_page_visible = false;
                 $("#btnCfg").text("配置");
+                console.log("已应用配置");
             });
 
         }
@@ -190,7 +205,7 @@ function hide_all_element(element_name){
 
 //获取excel数据
 function handleFile(event) {
-    console.log("handleFile")
+    console.log("选择文件")
     var file = event.target.files[0];
 
     if (file) {
@@ -207,7 +222,7 @@ function handleFile(event) {
             // 将工作表的数据转换成JSON对象
             var jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 2 });
             loaded_person_file = JSON.parse(JSON.stringify(jsonData));
-            console.log("loaded_person_file:", loaded_person_file);
+            console.log("已加载名单数据:", loaded_person_file);
             file="";
 
             // 打印JSON对象
@@ -228,8 +243,8 @@ function getRandomPerson() {
 
     // 从数组中移除已抽取的人员
     allPerson.splice(randomIndex, 1);
-    console.log("randomIndex:", randomIndex);
-    console.log("getRandomPerson:", allPerson);
+    console.log("随机数randomIndex:", randomIndex);
+    console.log("抽选到的人员信息:", selectedPerson);
 
     return selectedPerson;
 }
